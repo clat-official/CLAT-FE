839d346ca27865ce81d54c50e56e5eaa1c2c6d18
jmlee2147 <jm2147@khu.ac.kr>
2026-02-24 22:28:05 +0900

    feat: SVGR 세팅 및 아이콘 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
+import type { Configuration } from 'webpack'
 
 const withVanillaExtract = createVanillaExtractPlugin()
 
-const nextConfig = {}
+const nextConfig = {
+  webpack(config: Configuration) {
+    config.module!.rules!.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+}
 
 export default withVanillaExtract(nextConfig)
--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,16 +1,9 @@
+import PlusIcon from '@/assets/icons/icon-plus.svg'
 import Text from '@/components/common/Text'
 export default function HomePage() {
   return (
-    <div style={{ padding: '40px', display: 'flex', flexDirection: 'column', gap: '12px' }}>
-      <Text variant="display" as="h1" color='primary500'>display · 28px</Text>
-      <Text variant="headingLg" as="h2">headingLg · 24px</Text>
-      <Text variant="headingMd" as="h3">headingMd · 20px</Text>
-      <Text variant="headingSm" as="h4">headingSm · 18px</Text>
-      <Text variant="titleMd" as="p">titleMd · 16px</Text>
-      <Text variant="titleSm" as="p">titleSm · 14px</Text>
-      <Text variant="bodyLg" as="p">bodyLg · 16px</Text>
-      <Text variant="bodyMd" as="p">bodyMd · 14px</Text>
-      <Text variant="labelSm" as="span">labelSm · 12px</Text>
+    <div>
+      <PlusIcon width={24} height={24} />
     </div>
   )
 }
