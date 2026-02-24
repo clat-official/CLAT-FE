b8867c27f11377c4a90db351c8ebf33f855184a1
jmlee2147 <jm2147@khu.ac.kr>
2026-02-24 23:07:49 +0900

    feat: Button 구현

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,9 +1,7 @@
 import PlusIcon from '@/assets/icons/icon-plus.svg'
 import Text from '@/components/common/Text'
+import Button from '@/components/common/Button'
+
 export default function HomePage() {
-  return (
-    <div>
-      <PlusIcon width={24} height={24} />
-    </div>
-  )
+  return <div>홈</div>
 }
