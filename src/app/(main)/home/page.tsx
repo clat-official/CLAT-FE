806b5a9a9051fd2563b540fc73e413394849c5e8
jmlee2147 <jm2147@khu.ac.kr>
2026-03-02 23:21:56 +0900

    feat: Toast 구현

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,26 +1,13 @@
-import PlusIcon from '@/assets/icons/icon-plus.svg'
-import Text from '@/components/common/Text'
+'use client'
+
 import Button from '@/components/common/Button'
-import Input from '@/components/common/Input'
-import Textarea from '@/components/common/Textarea'
+import useToast from '@/hooks/useToast'
 
 export default function HomePage() {
+  const toast = useToast()
+
   return (
-    <div
-      style={{
-        padding: '40px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '12px',
-        maxWidth: '600px',
-      }}
-    >
-      <Input placeholder="예) 정규 수업 템플릿" />
-      <Input defaultValue="정규 수업 템플릿" />
-      <Input disabled placeholder="disabled" />
-      <Textarea placeholder="내용을 입력해주세요" />
-      <Textarea defaultValue="내용내용내용내용내용내용내용내용내용내용내용내용" />
-      <Textarea disabled placeholder="disabled" />
+    <div style={{ padding: '40px', display: 'flex', flexDirection: 'column', gap: '12px', maxWidth: '600px' }}>
     </div>
   )
 }
